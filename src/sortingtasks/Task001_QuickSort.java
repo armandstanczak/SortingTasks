8a4b9309c6373be14bfb572a16865f357fa575d3
Armand Stanczak <armandstanczak@gmail.com>
2020-06-02 22:30:40 +0200

    Sorting tasks

---
+++ src/sortingtasks/Task001_QuickSort.java
@@ -0,0 +1,13 @@
+package sortingtasks;
+
+public class Task001_QuickSort {
+
+    public static void main(String[] args) {
+
+        int[] a = {4, 6, 9, 1, 4, 2, 0, 5, 2, 7, 3, 1, 7};
+        int pivot = a.length - 1;
+        
+
+
+    }
+}
