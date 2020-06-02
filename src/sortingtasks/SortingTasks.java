11da3de9df1fbd8abd404d6f4ef171c56b81302a
Armand Stanczak <armandstanczak@gmail.com>
2020-06-02 21:40:31 +0200

    Sorting tasks

---
+++ src/sortingtasks/SortingTasks.java
@@ -0,0 +1,16 @@
+package sortingtasks;
+
+/**
+ *
+ * @author Armand Stańczak
+ */
+public class SortingTasks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
